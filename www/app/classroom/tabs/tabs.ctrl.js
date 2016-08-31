54d5978b5fe2a4d89078ec815730628ebbbdf1dc
Diego Rodrigues <diego.mrodrigues@outlook.com>
2016-08-31 16:35:59 -0300

    First COmmit

---
+++ www/app/classroom/tabs/tabs.ctrl.js
@@ -0,0 +1,24 @@
+// Controller of dashboard.
+appControllers.controller('classroomTabsCtrl', function ($scope, $timeout, $state,$stateParams, $ionicHistory) {
+
+    //$scope.isAnimated is the variable that use for receive object data from state params.
+    //For enable/disable row animation.
+    $scope.isAnimated =  $stateParams.isAnimated;
+
+    // navigateTo is for navigate to other page 
+    // by using targetPage to be the destination state. 
+    // Parameter :  
+    // stateNames = target state to go.
+    $scope.navigateTo = function (stateName, params) {
+        $timeout(function () {
+            if ($ionicHistory.currentStateName() != stateName) {
+                $ionicHistory.nextViewOptions({
+                    disableAnimate: false,
+                    disableBack: true
+                });
+                $state.go(stateName, params);
+            }
+        }, ($scope.isAnimated  ? 300 : 0));
+    }; // End of navigateTo.
+
+}); // End of dashboard controller.
